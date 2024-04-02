e685c9aa059e44ef307186fc105d789a0ce0edb9
Romain Delorme-Glorieux <romain.delorme-glorieux@signature.one>
2024-04-02 17:11:13 +0200

    Add parser for message 16

---
+++ Solutions/Ais.Net.Specs/Ais/Net/Specs/AisMessageTypes/AssignedModeCommandParserSpecs.feature
@@ -0,0 +1,29 @@
+# Copyright (c) Endjin Limited. All rights reserved.
+
+Feature: AssignedModeCommandParserSpecsSteps
+    In order process AIS messages from an nm4 file
+    As a developer
+    I want the NmeaAisAssignedModeCommandParser to be able to parse the payload section of message type 25: Assigned mode command
+    
+Scenario: Message Type
+    When I parse '@02=VgPoD@C43h00' with padding 0 as a Assigned mode command
+    Then NmeaAisAssignedModeCommandParser.Type is 16
+
+Scenario: Full message
+    When I parse '<payload>' with padding <padding> as a Assigned mode command
+    Then NmeaAisAssignedModeCommandParser.Type is <type>
+    And NmeaAisAssignedModeCommandParser.RepeatIndicator is <repeatindicator>
+    And NmeaAisAssignedModeCommandParser.Mmsi is <mmsi>
+    And NmeaAisAssignedModeCommandParser.SpareBits38 is <spare38>
+    And NmeaAisAssignedModeCommandParser.DestinationMmsiA is <destinationa>
+    And NmeaAisAssignedModeCommandParser.OffsetA is <offseta>
+    And NmeaAisAssignedModeCommandParser.IncrementA is <incrementa>
+    And NmeaAisAssignedModeCommandParser.DestinationMmsiB is <destinationb>
+    And NmeaAisAssignedModeCommandParser.OffsetB is <offsetb>
+    And NmeaAisAssignedModeCommandParser.IncrementB is <incrementb>
+    And NmeaAisAssignedModeCommandParser.SpareBitsAtEnd is <spareend>
+
+    Examples:
+    | payload                  | padding | type | repeatindicator | mmsi      | spare38 | destinationa | offseta | incrementa | destinationb | offsetb | incrementb | spareend |
+    | @02=VgPoD@C43h00         | 0       | 16   | 0               | 2320062   | 0       | 232014129    | 60      | 0          |              |         |            | 0        |
+    | @02a4KQD2cFP<P0000000500 | 0       | 16   | 0               | 2770030   | 0       | 352497000    | 200     | 0          | 0            | 20      | 0          |          |
---
+++ Solutions/Ais.Net.Specs/Ais/Net/Specs/AisMessageTypes/AssignedModeCommandParserSpecsSteps.cs
@@ -0,0 +1,99 @@
+﻿namespace Ais.Net.Specs.AisMessageTypes
+{
+    using System.Text;
+    using NUnit.Framework;
+    using TechTalk.SpecFlow;
+
+    [Binding]
+    public class AssignedModeCommandParserSpecsSteps
+    {
+        private ParserMaker makeParser;
+
+        private delegate NmeaAisAssignedModeCommandParser ParserMaker();
+
+        private delegate void ParserTest(NmeaAisAssignedModeCommandParser parser);
+
+        [When("I parse '(.*)' with padding (.*) as a Assigned mode command")]
+        public void WhenIParseWithNmeaAisAssignedModeCommandParser(string payload, uint padding)
+        {
+            this.When(() => new NmeaAisAssignedModeCommandParser(Encoding.ASCII.GetBytes(payload), padding));
+        }
+
+        [Then(@"NmeaAisAssignedModeCommandParser\.Type is (.*)")]
+        public void ThenNmeaAisAssignedModeCommandParser_TypeIs(MessageType messageType)
+        {
+            this.Then(parser => Assert.AreEqual(messageType, parser.MessageType));
+        }
+
+        [Then(@"NmeaAisAssignedModeCommandParser\.RepeatIndicator is (.*)")]
+        public void ThenNmeaAisAssignedModeCommandParser_RepeatIndicatorIs(uint repeatCount)
+        {
+            this.Then(parser => Assert.AreEqual(repeatCount, parser.RepeatIndicator));
+        }
+
+        [Then(@"NmeaAisAssignedModeCommandParser\.Mmsi is (.*)")]
+        public void ThenNmeaAisAssignedModeCommandParser_MmsiIs(uint mmsi)
+        {
+            this.Then(parser => Assert.AreEqual(mmsi, parser.Mmsi));
+        }
+
+        [Then(@"NmeaAisAssignedModeCommandParser\.SpareBits38 is (.*)")]
+        public void ThenNmeaAisAssignedModeCommandParser_Is(uint value)
+        {
+            this.Then(parser => Assert.AreEqual(value, parser.SpareBits38));
+        }
+
+        [Then(@"NmeaAisAssignedModeCommandParser\.DestinationMmsiA is (.*)")]
+        public void ThenNmeaAisAssignedModeCommandParser_DestinationMmsiAIs(uint value)
+        {
+            this.Then(parser => Assert.AreEqual(value, parser.DestinationMmsiA));
+        }
+
+        [Then(@"NmeaAisAssignedModeCommandParser\.OffsetA is (.*)")]
+        public void ThenNmeaAisAssignedModeCommandParser_OffsetAIs(uint value)
+        {
+            this.Then(parser => Assert.AreEqual(value, parser.OffsetA));
+        }
+
+        [Then(@"NmeaAisAssignedModeCommandParser\.IncrementA is (.*)")]
+        public void ThenNmeaAisAssignedModeCommandParser_IncrementAIs(uint value)
+        {
+            this.Then(parser => Assert.AreEqual(value, parser.IncrementA));
+        }
+
+        [Then(@"NmeaAisAssignedModeCommandParser\.DestinationMmsiB is (.*)")]
+        public void ThenNmeaAisAssignedModeCommandParser_DestinationMmsiBIs(uint? value)
+        {
+            this.Then(parser => Assert.AreEqual(value, parser.DestinationMmsiB));
+        }
+
+        [Then(@"NmeaAisAssignedModeCommandParser\.OffsetB is (.*)")]
+        public void ThenNmeaAisAssignedModeCommandParser_OffsetBIs(uint? value)
+        {
+            this.Then(parser => Assert.AreEqual(value, parser.OffsetB));
+        }
+
+        [Then(@"NmeaAisAssignedModeCommandParser\.IncrementB is (.*)")]
+        public void ThenNmeaAisAssignedModeCommandParser_IncrementBIs(uint? value)
+        {
+            this.Then(parser => Assert.AreEqual(value, parser.IncrementB));
+        }
+
+        [Then(@"NmeaAisAssignedModeCommandParser\.SpareBitsAtEnd is (.*)")]
+        public void ThenNmeaAisAssignedModeCommandParser_SpareBitsAtEndAIs(uint? value)
+        {
+            this.Then(parser => Assert.AreEqual(value, parser.SpareBitsAtEnd));
+        }
+
+        private void When(ParserMaker makeParser)
+        {
+            this.makeParser = makeParser;
+        }
+
+        private void Then(ParserTest test)
+        {
+            NmeaAisAssignedModeCommandParser parser = this.makeParser();
+            test(parser);
+        }
+    }
+}
