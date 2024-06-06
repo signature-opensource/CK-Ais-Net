1c0889057341cf7de8f0b0e5a458acdf4ee20c68
Romain Delorme-Glorieux <romain.delorme-glorieux@signature.one>
2024-06-06 11:06:21 +0200

    Expose PipeReader to NmeaStreamParser.ParseAsync

--- CK.Ais.Net/NmeaStreamParser.cs
+++ CK.Ais.Net/NmeaStreamParser.cs
@@ -100,10 +100,23 @@ namespace Ais.Net
         /// <param name="processor">Handler for the parsed lines.</param>
         /// <param name="options">Configures parser behaviour.</param>
         /// <returns>A task that completes when the stream has been processed.</returns>
-        public static async Task ParseStreamAsync(
+        public static Task ParseStreamAsync(
             Stream stream,
             INmeaLineStreamProcessor processor,
             NmeaParserOptions options)
+        {
+            var reader = PipeReader.Create(stream, new StreamPipeReaderOptions(bufferSize: 64 * 1024));
+            return ParseAsync( reader, processor, options );
+        }
+
+        /// <summary>
+        /// Process the contents of a pipe reader.
+        /// </summary>
+        /// <param name="reader">The reader to process.</param>
+        /// <param name="processor">Handler for the parsed lines.</param>
+        /// <param name="options">Configures parser behaviour.</param>
+        /// <returns>A task that completes when the stream has been processed.</returns>
+        public static async Task ParseAsync( PipeReader reader, INmeaLineStreamProcessor processor, NmeaParserOptions options )
         {
             int lines = 0;
             int ticksAtStart = Environment.TickCount;
@@ -111,17 +124,16 @@ namespace Ais.Net
 
             try
             {
-                PipeReader reader = CreateFileReader(stream);
 
                 byte[] splitLineBuffer = new byte[1000];
-                while (true)
+                while( true )
                 {
-                    ReadResult result = await reader.ReadAsync().ConfigureAwait(false);
-                    ReadOnlySequence<byte> remainingSequence = ProcessBuffer(result);
+                    ReadResult result = await reader.ReadAsync().ConfigureAwait( false );
+                    ReadOnlySequence<byte> remainingSequence = ProcessBuffer( result );
 
-                    reader.AdvanceTo(remainingSequence.Start, remainingSequence.End);
+                    reader.AdvanceTo( remainingSequence.Start, remainingSequence.End );
 
-                    if (result.IsCompleted)
+                    if( result.IsCompleted )
                     {
                         break;
                     }
@@ -136,55 +148,55 @@ namespace Ais.Net
                     lines,
                     totalTicks,
                     lines % LineCountInterval,
-                    finalTicks - ticksAtLastLineCount);
+                    finalTicks - ticksAtLastLineCount );
 
                 ReadOnlySequence<byte> ProcessBuffer(
-                    in ReadResult result)
+                    in ReadResult result )
                 {
                     ReadOnlySpan<byte> lineSpan;
                     SequencePosition? position = null;
 
                     ReadOnlySequence<byte> remainingSequence = result.Buffer;
 
-                    while ((position = remainingSequence.PositionOf((byte)'\n') ?? (remainingSequence.IsEmpty || !result.IsCompleted ? default(SequencePosition?) : remainingSequence.End)) != null)
+                    while( (position = remainingSequence.PositionOf( (byte)'\n' ) ?? (remainingSequence.IsEmpty || !result.IsCompleted ? default( SequencePosition? ) : remainingSequence.End)) != null )
                     {
-                        ReadOnlySequence<byte> line = remainingSequence.Slice(remainingSequence.Start, position.Value);
+                        ReadOnlySequence<byte> line = remainingSequence.Slice( remainingSequence.Start, position.Value );
 
-                        if (line.IsSingleSegment)
+                        if( line.IsSingleSegment )
                         {
                             lineSpan = line.First.Span;
                         }
                         else
                         {
                             Span<byte> reassemblySpan = splitLineBuffer;
-                            line.CopyTo(reassemblySpan);
-                            lineSpan = reassemblySpan.Slice(0, (int)line.Length);
+                            line.CopyTo( reassemblySpan );
+                            lineSpan = reassemblySpan.Slice( 0, (int)line.Length );
                         }
 
-                        if (lineSpan.Length > 0 && lineSpan[lineSpan.Length - 1] == (byte)'\r')
+                        if( lineSpan.Length > 0 && lineSpan[lineSpan.Length - 1] == (byte)'\r' )
                         {
-                            lineSpan = lineSpan.Slice(0, lineSpan.Length - 1);
+                            lineSpan = lineSpan.Slice( 0, lineSpan.Length - 1 );
                         }
 
-                        if (lineSpan.Length > 0)
+                        if( lineSpan.Length > 0 )
                         {
                             try
                             {
-                                var parsedLine = new NmeaLineParser(lineSpan, options.ThrowWhenTagBlockContainsUnknownFields, options.TagBlockStandard);
+                                var parsedLine = new NmeaLineParser( lineSpan, options.ThrowWhenTagBlockContainsUnknownFields, options.TagBlockStandard );
 
-                                processor.OnNext(parsedLine, lines + 1);
+                                processor.OnNext( parsedLine, lines + 1 );
                             }
-                            catch (Exception x)
+                            catch( Exception x )
                             {
-                                processor.OnError(lineSpan, x, lines + 1);
+                                processor.OnError( lineSpan, x, lines + 1 );
                             }
                         }
 
-                        remainingSequence = position.Value.Equals(remainingSequence.End)
-                            ? remainingSequence.Slice(remainingSequence.End)
-                            : remainingSequence.Slice(remainingSequence.GetPosition(1, position.Value));
+                        remainingSequence = position.Value.Equals( remainingSequence.End )
+                            ? remainingSequence.Slice( remainingSequence.End )
+                            : remainingSequence.Slice( remainingSequence.GetPosition( 1, position.Value ) );
 
-                        if (++lines % LineCountInterval == 0)
+                        if( ++lines % LineCountInterval == 0 )
                         {
                             int currentTicks = Environment.TickCount;
                             int ticksSinceLastLineCount = currentTicks - ticksAtLastLineCount;
@@ -194,7 +206,7 @@ namespace Ais.Net
                                 lines,
                                 currentTicks - ticksAtStart,
                                 LineCountInterval,
-                                ticksSinceLastLineCount);
+                                ticksSinceLastLineCount );
                             ticksAtLastLineCount = currentTicks;
                         }
                     }
@@ -206,50 +218,6 @@ namespace Ais.Net
             {
                 processor.OnCompleted();
             }
-        }
-
-        private static PipeReader CreateFileReader(Stream file)
-        {
-            async Task ProcessFileAsync(PipeWriter writer)
-            {
-                try
-                {
-                    while( true )
-                    {
-                        Memory<byte> memory = writer.GetMemory();
-
-                        // See https://github.com/dotnet/corefx/blob/master/src/Common/src/CoreLib/System/IO/Stream.cs#L379
-                        // for how Stream.ReadAsync handles Memory<byte>.
-#if NETSTANDARD2_0
-                        System.Runtime.InteropServices.MemoryMarshal.TryGetArray(memory, out ArraySegment<byte> memoryArray);
-                        int read = await file.ReadAsync(memoryArray.Array, memoryArray.Offset, memoryArray.Count).ConfigureAwait(false);
-#else
-                        int read = await file.ReadAsync( memory ).ConfigureAwait( false );
-#endif
-                        if( read == 0 )
-                        {
-                            break;
-                        }
-
-                        writer.Advance( read );
-
-                        await writer.FlushAsync().ConfigureAwait( false );
-                    }
-                }
-                catch( Exception ex )
-                {
-                    await writer.CompleteAsync( ex );
-                }
-                finally
-                {
-                    await writer.CompleteAsync();
-                }
-            }
-
-            var pipe = new Pipe(new PipeOptions(minimumSegmentSize: 64 * 1024));
-            _ = ProcessFileAsync(pipe.Writer);
-
-            return pipe.Reader;
         }
     }
 }
